5bd9057bc182523e265ee0c8b7ebc901a9e6530a
Akshat Bisht <109125241+Kaisama@users.noreply.github.com>
2024-05-14 16:00:28 +0530

    Delete ECommerce-Website directory

--- ECommerce-Website/frontend/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Loader = () => {
-  return (
-    <div className="w-16 h-16 border-t-4 border-pink-500 border-opacity-50 rounded-full animate-spin"></div>
-  )
-}
-
-export default Loader
